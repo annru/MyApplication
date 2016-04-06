9983fd8bf668017a0fbe48c1624f7925a7a3e33c
annru <annnru@163.com>
2016-04-06 20:46:32 +0800

    新增Fragment嵌套Fragment

---
+++ anzhixiangDemo/src/main/java/com/example/myapplication/fragment/ProductFragment.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+/**
+ * Created by annru on 2016/4/6.
+ * Description:投资理财产品列表
+ */
+public class ProductFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_product, container, false);
+    }
+}
--- anzhixiangDemo/src/main/java/com/example/myapplication/fragment/Tab2Fragment.java
+++ anzhixiangDemo/src/main/java/com/example/myapplication/fragment/Tab2Fragment.java
@@ -2,30 +2,83 @@ package com.example.myapplication.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import com.example.myapplication.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class Tab2Fragment extends Fragment {
+
+public class Tab2Fragment extends Fragment implements RadioGroup.OnCheckedChangeListener {
     private static final String TAG = "Tab2Fragment";
+
+    private FragmentTransaction transaction;
+    private ProductFragment productFragment;
+    private OrgFragment orgFragment;
+    private Fragment currentFragment;
+
+    @Bind(R.id.product_radio_group)
+    RadioGroup radioGroup;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate");
+        init();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.i(TAG, "onCreateView");
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab2, container, false);
+        ButterKnife.bind(this, view);
+        radioGroup.setOnCheckedChangeListener(this);
+        return view;
+    }
+
+    private void init() {
+        if (null == productFragment)
+            productFragment = new ProductFragment();
+        transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.product_content_layout, productFragment);
+        transaction.commit();
+        currentFragment = productFragment;
     }
 
 
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        transaction = getChildFragmentManager().beginTransaction();
+        transaction.hide(currentFragment);
+        switch (checkedId) {
+            case R.id.product_btn:
+                if (null == productFragment) {
+                    productFragment = new ProductFragment();
+                    transaction.add(R.id.product_content_layout, productFragment);
+                } else {
+                    transaction.show(productFragment);
+                }
+                currentFragment = productFragment;
+
+                break;
+            case R.id.org_btn:
+                if (null == orgFragment) {
+                    orgFragment = new OrgFragment();
+                    transaction.add(R.id.product_content_layout, orgFragment);
+                } else {
+                    transaction.show(orgFragment);
+                }
+                currentFragment = orgFragment;
+                break;
+        }
+        transaction.commit();
+    }
 }
