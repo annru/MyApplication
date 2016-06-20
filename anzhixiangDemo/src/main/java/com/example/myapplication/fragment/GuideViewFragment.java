3718407c9aca3dcda597085f2ee9fc49a7e1551a
annru <annnru@163.com>
2016-06-20 21:41:15 +0800

    新增

---
+++ anzhixiangDemo/src/main/java/com/example/myapplication/fragment/GuideViewFragment.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.fragment;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+import com.example.myapplication.activity.GuideActivity;
+import com.example.myapplication.base.BaseFragment;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * 引导页
+ */
+public class GuideViewFragment extends BaseFragment implements View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @OnClick(R.id.guide_btn)
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getActivity(), GuideActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ anzhixiangDemo/src/main/java/com/example/myapplication/fragment/MainFragment.java
@@ -0,0 +1,40 @@
+package com.example.myapplication.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+import com.example.myapplication.base.BaseFragment;
+
+/**
+ * A fragment with a Google +1 button.
+ */
+public class MainFragment extends BaseFragment {
+
+
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+
+}
