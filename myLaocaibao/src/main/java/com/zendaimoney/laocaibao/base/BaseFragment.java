f9da3a8ea12b14ea62efabdb57237c4c6af6c793
安志翔 <anzx@zendaimoney.com>
2017-06-13 17:54:42 +0800

    init

---
+++ myLaocaibao/src/main/java/com/zendaimoney/laocaibao/base/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.zendaimoney.laocaibao.base;
+
+/**
+ * Created by 00224524 on 2017/6/13.
+ * description:
+ */
+
+public class BaseFragment {
+}
---
+++ myLaocaibao/src/main/java/com/zendaimoney/laocaibao/fragment/ProductFragment.java
@@ -0,0 +1,13 @@
+package com.zendaimoney.laocaibao.fragment;
+
+import com.zendaimoney.laocaibao.base.BaseFragment;
+
+/**
+ * Created by 00224524 on 2017/6/13.
+ * description:
+ */
+
+public class ProductFragment extends BaseFragment {
+
+
+}
