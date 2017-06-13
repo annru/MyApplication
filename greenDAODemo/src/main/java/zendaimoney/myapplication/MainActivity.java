da4ffa39ac5f130c7ce266c180c387f998b39fac
安志翔 <anzx@zendaimoney.com>
2017-06-13 11:01:49 +0800

    commit 4

--- greenDAODemo/src/main/java/zendaimoney/myapplication/MainActivity.java
+++ greenDAODemo/src/main/java/zendaimoney/myapplication/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         //git commit 2
+        //git commit 4
     }
 
 
